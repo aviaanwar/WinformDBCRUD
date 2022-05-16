d522c0c4f77c108f97ff6b3611ad136ac77cb578
aviaanwar <avia.anwar.anika154@gmail.com>
2022-05-16 15:54:33 +0500

    Initial Commit

---
+++ CrudInGridView/Default.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CrudInGridView
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string connectionString = @"Data Source=.\SQLEXPRESS;Database=MyDatabase;Trusted_Connection=True;"; //for MS SQL. Poiro vitore taile bujhba etar meaning ki.
+        //string connectionString = @"Data Source=G:\Gridview Insert Update and Delete\CrudInGridView\CrudInGridView\DummyDB.db;Version=3;"; //for SQLite
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                PopulateGridview();
+            }
+        }
+
+        void PopulateGridview()
+        {
+            DataTable dtbl = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(connectionString)) //Joto jaygay Sqlite ase shob Sql korte hobe
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM DummyTable", sqlCon);
+                sqlDa.Fill(dtbl);
+            }
+            if (dtbl.Rows.Count > 0)
+            {
+                gvPhoneBook.DataSource = dtbl;
+                gvPhoneBook.DataBind();
+            }
+            else
+            {
+                dtbl.Rows.Add(dtbl.NewRow());
+                gvPhoneBook.DataSource = dtbl;
+                gvPhoneBook.DataBind();
+                gvPhoneBook.Rows[0].Cells.Clear();
+                gvPhoneBook.Rows[0].Cells.Add(new TableCell());
+                gvPhoneBook.Rows[0].Cells[0].ColumnSpan = dtbl.Columns.Count;
+                gvPhoneBook.Rows[0].Cells[0].Text = "No Data Found ..!";
+                gvPhoneBook.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+            }
+
+        }
+
+        protected void gvPhoneBook_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            try
+            {
+                if (e.CommandName.Equals("AddNew"))
+                {
+                    using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                    {
+                        sqlCon.Open();
+                        string query = "INSERT INTO DummyTable (FirstName,LastName,Email,Age) VALUES (@FirstName,@LastName,@Email,@Age)";
+                        SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                        sqlCmd.Parameters.AddWithValue("@FirstName", (gvPhoneBook.FooterRow.FindControl("txtFirstNameFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@LastName", (gvPhoneBook.FooterRow.FindControl("txtLastNameFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Email", (gvPhoneBook.FooterRow.FindControl("txtContactFooter") as TextBox).Text.Trim());
+                        sqlCmd.Parameters.AddWithValue("@Age", (gvPhoneBook.FooterRow.FindControl("txtEmailFooter") as TextBox).Text.Trim());
+                        sqlCmd.ExecuteNonQuery();
+                        PopulateGridview();
+                        lblSuccessMessage.Text = "New Record Added";
+                        lblErrorMessage.Text = "";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+
+        protected void gvPhoneBook_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvPhoneBook.EditIndex = e.NewEditIndex;
+            PopulateGridview();
+        }
+
+        protected void gvPhoneBook_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvPhoneBook.EditIndex = -1;
+            PopulateGridview();
+        }
+
+        protected void gvPhoneBook_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    string query = "UPDATE DummyTable SET FirstName=@FirstName,LastName=@LastName,Email=@Email,Age=@Age WHERE ID = @id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@FirstName", (gvPhoneBook.Rows[e.RowIndex].FindControl("txtFirstName") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@LastName", (gvPhoneBook.Rows[e.RowIndex].FindControl("txtLastName") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Email", (gvPhoneBook.Rows[e.RowIndex].FindControl("txtContact") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Age", (gvPhoneBook.Rows[e.RowIndex].FindControl("txtEmail") as TextBox).Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@id",Convert.ToInt32(gvPhoneBook.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    gvPhoneBook.EditIndex = -1;
+                    PopulateGridview();
+                    lblSuccessMessage.Text = "Selected Record Updated";
+                    lblErrorMessage.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+
+        protected void gvPhoneBook_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    string query = "DELETE FROM DummyTable WHERE ID = @id";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@id", Convert.ToInt32(gvPhoneBook.DataKeys[e.RowIndex].Value.ToString()));
+                    sqlCmd.ExecuteNonQuery();
+                    PopulateGridview();
+                    lblSuccessMessage.Text = "Selected Record Deleted";
+                    lblErrorMessage.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblSuccessMessage.Text = "";
+                lblErrorMessage.Text = ex.Message;
+            }
+        }
+    }
+}
